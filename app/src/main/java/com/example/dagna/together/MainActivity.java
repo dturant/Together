6e736ac285c1966b8f22c881d1556ceb160f2925
dagna <dagna>
2016-03-21 12:23:07 +0100

    database connection (to fix)

--- app/src/main/java/com/example/dagna/together/MainActivity.java
+++ app/src/main/java/com/example/dagna/together/MainActivity.java
@@ -4,6 +4,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +44,39 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void register(View view){
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //String host="jdbc:jtds:sqlserver://http://project-together.cba.pl/project_together_cba_pl";
+            //String name="ctb";
+            //String password="ifejestfajne";
+            String database="project_together_cba_pl";
+            String server="mysql.cba.pl";
+            String user="ctb";
+            String password="ifejestfajne";
+
+            //Connection conn = DriverManager.getConnection(host, name, password);
+
+            String ConnectionURL = "jdbc:jtds:sqlserver://" + server + ";"
+                    + "databaseName=" + database + ";user=" + user
+                    + ";password=" + password + ";";
+            Connection conn = DriverManager.getConnection(ConnectionURL);
+            Statement stmt = conn.createStatement( );
+            String SQL = "SELECT * FROM user";
+            ResultSet rs = stmt.executeQuery( SQL );
+
+            String login = rs.getString("login");
+
+            EditText user_login   = (EditText)findViewById(R.id.text);
+            user_login.setText("login", TextView.BufferType.EDITABLE);
+
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.print("Tu jest blad");
+        }
     }
 }
